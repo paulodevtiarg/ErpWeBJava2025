460a0982518712cf46f0e9e0d9d9cf8bf7d93929
paulodevtiarg <prn.dev.ti@gmail.com>
2025-06-10 12:52:33 -0300

    PRIMEIRO COMMIT

---
+++ ElarisERP/src/main/java/br/com/elariserp/ElarisErpApplication.java
@@ -0,0 +1,13 @@
+package br.com.elariserp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElarisErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElarisErpApplication.class, args);
+	}
+
+}
